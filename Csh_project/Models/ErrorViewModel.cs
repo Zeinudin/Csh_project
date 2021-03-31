3872d8e3eeed8027ee5bb7523a9cc6e8a7376eb4
Zeinudin <lirov_z@iuca.kg>
2021-03-31 15:07:22 +0600

    end thrid lab

---
+++ Csh_project/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Csh_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+}
